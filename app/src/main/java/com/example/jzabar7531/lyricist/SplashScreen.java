ab2d315a453d367315079184fd0fa5fd13eeee79
Jordan Zabar <jordan-zabar77@hotmail.com>
2017-09-29 14:01:29 -0400

    Added a Splash Screen

---
+++ app/src/main/java/com/example/jzabar7531/lyricist/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.example.jzabar7531.lyricist;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,LoginActivity.class));
+
+            }
+        },1000);
+    }
+}
